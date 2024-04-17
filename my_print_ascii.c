30e52059dbbdda14e133809f3b6406f820d5e5ca
SheyZzen <167293983+SheyZzen@users.noreply.github.com>
2024-04-17 12:36:25 +0000

    print ascii version 12

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -3,7 +3,7 @@ void print_char(char c);
 void my_print_ascii(void)
 {
             int indice = 33;
-            while (32 < indice && indice < 128){
+            while (32 < indice && indice < 127){
             print_char(indice);
             indice++;
             }
